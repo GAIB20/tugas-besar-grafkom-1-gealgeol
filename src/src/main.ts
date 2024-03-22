820a1932e7284a97db8268757aae725de8c0033d
liviaarumsari <13521094@mahasiswa.itb.ac.id>
2024-03-22 19:08:13 +0700

    feat: setup main program

--- src/src/main.ts
+++ src/src/main.ts
@@ -1,71 +1,53 @@
+// @ts-nocheck
 import './style.css';
 import { VertexShaderSource } from './shaders/vertex-shader.ts';
 import { FragmentShaderSource } from './shaders/fragment-shader.ts';
 import { resizeCanvasToDisplaySize, createProgram, createShader } from './utils/web-gl.ts';
+import { Point } from './classes/point.ts';
+import { Line } from './classes/line.ts';
+import { Shape } from './classes/shape.ts';
 
-function mainProgram() {
-  let canvas: HTMLCanvasElement | null = document.querySelector<HTMLCanvasElement>('#webgl-canvas');
-  if (!canvas) {
-    console.log('no canvas');
-    return;
+// Create WebGL program
+let canvas: HTMLCanvasElement | null = document.querySelector<HTMLCanvasElement>('#webgl-canvas');
+resizeCanvasToDisplaySize(<HTMLCanvasElement>canvas)
+let gl = canvas!.getContext('webgl');
+
+let vertexShader = createShader(gl, gl.VERTEX_SHADER, VertexShaderSource);
+let fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, FragmentShaderSource);
+
+let program = createProgram(gl, vertexShader, fragmentShader);
+
+
+// Global variables
+let isDrawing = false;
+let objects: Shape[] = []
+canvas?.addEventListener("mousedown", (e) => {
+  const x = e.clientX
+  const y = e.clientY
+  const point = new Point(x, y)
+  console.log(x, y);
+  if (!isDrawing) {
+    const line = new Line(objects.length, point);
+    objects.push(line);
+
+    isDrawing = true;
+  } else {
+    const line = objects[objects.length-1] as Line;
+    line.setEndPoint(point);
+    line.render(gl, program)
+
+    isDrawing = false;
   }
-  let gl = canvas.getContext('webgl');
-  if (!gl) {
-    console.log('no webgl');
-    return;
-  }
-  let vertexShader = createShader(gl, gl.VERTEX_SHADER, VertexShaderSource);
-  let fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, FragmentShaderSource);
-  if (!vertexShader || !fragmentShader) {
-    console.log('no shader');
-    return;
-  }
-  let program = createProgram(gl, vertexShader, fragmentShader);
-  if (!program) {
-    return;
-  }
-  let positionAttributeLocation = gl.getAttribLocation(program, 'a_position');
 
-  let positionBuffer = gl.createBuffer();
+})
 
-  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+canvas?.addEventListener("mousemove", (e) => {
+  const x = e.clientX;
+  const y = e.clientY;
+  const point = new Point(x, y)
+  console.log(objects);
 
-  let positions = [
-    0, 0,
-    0, 0.5,
-    0.7, 0,
-  ];
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
-
-  resizeCanvasToDisplaySize(<HTMLCanvasElement>gl.canvas);
-
-  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-
-  gl.clearColor(0, 0, 0, 0);
-  gl.clear(gl.COLOR_BUFFER_BIT);
-
-  gl.useProgram(program);
-
-  gl.enableVertexAttribArray(positionAttributeLocation);
-
-  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-
-  // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
-  let size = 2;          // 2 components per iteration
-  let type = gl.FLOAT;   // the data is 32bit floats
-  let normalize = false; // don't normalize the data
-  let stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
-  let offset = 0;        // start at the beginning of the buffer
-  gl.vertexAttribPointer(
-    positionAttributeLocation, size, type, normalize, stride, offset);
-
-  // draw
-  let primitiveType = gl.TRIANGLES;
-  offset = 0;
-  let count = 3;
-  gl.drawArrays(primitiveType, offset, count);
-}
-
-mainProgram();
-
-
+  const line: Line = objects[objects.length-1] as Line;
+  line.setEndPoint(point);
+  line.render(gl, program);
+});
