728720b01d6e1a7e1b2eebcabfe63cd3d0b6e7d1
Alexander Jason <alexanderjason526@gmail.com>
2024-04-05 23:18:19 +0700

    fix: color input

--- src/src/utils/tools.ts
+++ src/src/utils/tools.ts
@@ -1,12 +1,15 @@
 function componentToHex(c: number) {
-  var hex = c.toString(16);
+  // Convert from normalized (0 to 1) range to 0-255 range and round
+  const scaledValue = Math.round(c * 255);
+  const hex = scaledValue.toString(16);
   return hex.length === 1 ? `0${hex}` : hex;
 }
+
 function rgbToHex(rgba: number[]): string {
+  // Assuming the input is an array of normalized RGB values (and optionally an alpha value)
   const [r, g, b] = rgba;
   return `#${componentToHex(r)}${componentToHex(g)}${componentToHex(b)}`;
 }
-
 function hexToRgb(hex: string):number[] {
   const r = parseInt(hex.slice(1, 3), 16);
   const g = parseInt(hex.slice(3, 5), 16);
